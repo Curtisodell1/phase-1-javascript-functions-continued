75159a859696006634af251311ba059b88e11980
Curtis O'Dell <curtisodell1@gmail.com>
2023-07-10 23:11:57 -0400

    some freaky functions

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate"){
+    return(`This Saturday, I want to ${activity}!`)
+}
+saturdayFun()
+saturdayFun("bathe my dog")
+
+function mondayWork(activity = "go to the office"){
+    return (`This Monday, I will ${activity}.`)
+}
+
+let wrapAdjective = function(flair = "*"){
+    return function (phrase = "special"){
+        return (`You are ${flair}${phrase}${flair}!`)
+    }
+}
+
+wrapAdjective("%")("a dedicated programmer")
